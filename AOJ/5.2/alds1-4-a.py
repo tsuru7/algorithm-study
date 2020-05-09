ac9463e978ceba889096c3b3c2dfe771f2457f00
Bokukko <bokukko7@gmail.com>
2020-05-09 09:27:14 +0900

    AC

--- AOJ/5.2/alds1-4-a.py
+++ AOJ/5.2/alds1-4-a.py
@@ -15,7 +15,24 @@ def main(n,s,q,t):
 
     return c
 
+def main2(n,s,q,t):
+    c=0
+    for tj in t:
+        s.append(tj)
+        i=0
+        while(s[i]!=tj):
+            i+=1
+        if(i<n):
+            c+=1
+        s.pop()
+    return c
+
+def main3(n,s,q,t):
+    sets = set(s)
+    sett = set(t)
+    return (len((sets & sett)))
+
 if __name__=='__main__':
     n,s,q,t = readinput()
-    ans = main(n,s,q,t)
+    ans = main3(n,s,q,t)
     print(ans)
