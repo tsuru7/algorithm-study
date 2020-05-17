6cf89bbf865e7298274d71e7c8eb3df27c9bb2e7
Bokukko <bokukko7@gmail.com>
2020-05-17 15:29:41 +0900

    AC

--- AtCoder/ABC167/C.py
+++ AtCoder/ABC167/C.py
@@ -21,6 +21,7 @@ def makeTableA(dat,n,m):
         #print(tbl) 
     return tbl  
 
+
 def main(n,m,x,dat):
     tblA=makeTableA(dat,n,m)
     #print(tblA)
@@ -53,7 +54,45 @@ def main(n,m,x,dat):
     return Pmin
 
 
+def readinput2():
+    n,m,x = list(map(int,input().split()))
+    tblA=[]
+    c=[]
+    for i in range(n):
+        s=list(map(int,input().split()))
+        c.append(s[0])
+        tblA.append(s[1:])
+    #print(c,tblA)
+    return n,m,x,c,tblA
+
+def main2(n,m,x,c,a):
+    tblB = [[0 for j in range(m+2)] for i in range(2**n)]
+    for i in range(2**n):
+        nn=1
+        for j in range(n):
+            #print(i,nn)
+            if (i&nn==nn):
+                for k in range(m):
+                    tblB[i][k]+=a[j][k]
+                tblB[i][m]+=c[j]
+            nn=nn*2
+        tblB[i][m+1]=min(tblB[i][:m])
+        #print(tblB)
+
+    Pmin=12*10**5+1
+    for i in range(2**n):
+        if(tblB[i][m+1]>=x):
+            price=tblB[i][m]
+            if(price < Pmin):
+                Pmin=price
+    if(Pmin==12*10**5+1):
+        Pmin=-1
+    return Pmin
+
+
 if __name__=='__main__':
-    n,m,x,dat=readinput()
-    ans=main(n,m,x,dat)
+    #n,m,x,dat=readinput()
+    #ans=main(n,m,x,dat)
+    n,m,x,c,tblA=readinput2()
+    ans=main2(n,m,x,c,tblA)
     print(ans)
